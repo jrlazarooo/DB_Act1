24b7cf014b786ad64b83c5fad1ba18e33a7d5db6
jrlazarooo <162771024+jrlazarooo@users.noreply.github.com>
2024-10-09 15:12:45 +0800

    Add files via upload

---
+++ DB_Act1_046_Agcanas/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace DB_Act1_046_Agcanas
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtKeyword = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.cboBrand = new System.Windows.Forms.ComboBox();
+            this.dtgrResults = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgrResults)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtKeyword
+            // 
+            this.txtKeyword.Location = new System.Drawing.Point(12, 12);
+            this.txtKeyword.Name = "txtKeyword";
+            this.txtKeyword.Size = new System.Drawing.Size(204, 20);
+            this.txtKeyword.TabIndex = 0;
+            this.txtKeyword.TextChanged += new System.EventHandler(this.txtKeyword_TextChanged);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(12, 38);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(93, 38);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 2;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // cboBrand
+            // 
+            this.cboBrand.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboBrand.FormattingEnabled = true;
+            this.cboBrand.Items.AddRange(new object[] {
+            "Acura",
+            "Aston Martin",
+            "Audi",
+            "Bentley",
+            "BMW",
+            "Buick",
+            "Cadillac",
+            "Chevrolet",
+            "Chrysler",
+            "Dodge",
+            "Eagle",
+            "Ford",
+            "Geo",
+            "GMC",
+            "Honda",
+            "Hummer",
+            "Hyundai",
+            "Infiniti",
+            "Isuzu",
+            "Jeep",
+            "Kia",
+            "Land Rover",
+            "Lexus",
+            "Lincoln",
+            "Lotus",
+            "Maserati",
+            "Mazda",
+            "Mercedes-Benz",
+            "Mercury",
+            "Mitsubishi",
+            "Nissan",
+            "Oldsmobile",
+            "Plymouth",
+            "Pontiac",
+            "Porsche",
+            "Ram",
+            "Saab",
+            "Saturn",
+            "Scion",
+            "Shelby",
+            "Spyker",
+            "Subaru",
+            "Suzuki",
+            "Toyota",
+            "Volkswagen",
+            "Volvo"});
+            this.cboBrand.Location = new System.Drawing.Point(222, 12);
+            this.cboBrand.Name = "cboBrand";
+            this.cboBrand.Size = new System.Drawing.Size(147, 21);
+            this.cboBrand.TabIndex = 3;
+            // 
+            // dtgrResults
+            // 
+            this.dtgrResults.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dtgrResults.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgrResults.Location = new System.Drawing.Point(12, 67);
+            this.dtgrResults.MultiSelect = false;
+            this.dtgrResults.Name = "dtgrResults";
+            this.dtgrResults.ReadOnly = true;
+            this.dtgrResults.RowHeadersVisible = false;
+            this.dtgrResults.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dtgrResults.Size = new System.Drawing.Size(567, 349);
+            this.dtgrResults.TabIndex = 4;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(590, 429);
+            this.Controls.Add(this.dtgrResults);
+            this.Controls.Add(this.cboBrand);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtKeyword);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgrResults)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtKeyword;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.ComboBox cboBrand;
+        private System.Windows.Forms.DataGridView dtgrResults;
+    }
+}
+
---
+++ DB_Act1_046_Agcanas/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+
+namespace DB_Act1_046_Agcanas
+{
+    public partial class frmMain : Form
+    {
+        string connectionStr = "server=localhost; database=db_act1; uid=root; pwd=uslt; port=3306";
+        MySqlConnection conn;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            conn = new MySqlConnection(connectionStr);
+            string query = "select * from car where model like '%" + txtKeyword.Text + "%' and brand like '%" + cboBrand.Text + "%'";
+            conn.Open();
+            MySqlDataAdapter dataAdapter = new MySqlDataAdapter(query, conn);
+            dataAdapter.Fill(dt);
+            conn.Close();
+            dtgrResults.DataSource = dt;
+            dtgrResults.Columns["ID"].Visible = false;
+            dtgrResults.Columns[1].HeaderText = "Model";
+            dtgrResults.Columns[2].HeaderText = "Brand";
+            dtgrResults.Columns[3].HeaderText = "Year";
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtKeyword.Clear();
+            cboBrand.SelectedIndex = -1;
+            dtgrResults.DataSource = null;
+        }
+
+        private void txtKeyword_TextChanged(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+            conn = new MySqlConnection(connectionStr);
+            string query = "select * from car where model like '%" + txtKeyword.Text + "%' and brand like '%" + cboBrand.Text + "%'";
+            conn.Open();
+            MySqlDataAdapter dataAdapter = new MySqlDataAdapter(query, conn);
+            dataAdapter.Fill(dt);
+            conn.Close();
+            dtgrResults.DataSource = dt;
+            dtgrResults.Columns["ID"].Visible = false;
+            dtgrResults.Columns[1].HeaderText = "Model";
+            dtgrResults.Columns[2].HeaderText = "Brand";
+            dtgrResults.Columns[3].HeaderText = "Year";
+        }
+    }
+}
